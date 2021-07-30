ba5eeea26d65eecfea91daa288b510ab88da4a22
Kooin-Shin <chaos930@innotree.com>
2021-07-30 17:18:56 +0900

    master

--- src/test/java/org/chaostocosmos/net/tcpproxy/TCPProxyTest.java
+++ src/test/java/org/chaostocosmos/net/tcpproxy/TCPProxyTest.java
@@ -3,7 +3,7 @@ package org.chaostocosmos.net.tcpproxy;
 public class TCPProxyTest {
     
     public static void main(String[] args) throws Exception {
-        String configPath = "D:\\Projects\\TCPProxy\\config.yml";
+        String configPath = "D:\\Github\\TCPProxy\\config.yml";
         TCPProxy tcpProxy = new TCPProxy(configPath);
         tcpProxy.startProxy();        
     }
